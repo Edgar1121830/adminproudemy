43d2fdd5a06f703876bc8ce73bea3c2143cd2b85
Edgar1121830 <edgarmedinauptc@gmail.com>
2018-11-19 19:51:43 -0500

    modificacion en modulos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 
-import { FunctionCall } from '@angular/compiler';
-import { PagesComponent } from './pages/pages.component';
-
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { PrigressComponent } from './pages/prigress/prigress.component';
-import { Grafics1Component } from './pages/grafics1/grafics1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 import { RegisterComponent } from './login/register.component';
 
 
 
 const appRoutes: Routes = [
-    {
-        path: '',
-        component: PagesComponent,
-        children: [
-            { path: 'dashboard', component: DashboardComponent},
-            { path: 'prigress', component: PrigressComponent},
-            { path: 'grafics1', component: Grafics1Component},
-            { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
-        ]
-    },
     { path: 'login', component: LoginComponent},
     { path: 'register', component: RegisterComponent},
     { path: '**', component: NopagefoundComponent}
